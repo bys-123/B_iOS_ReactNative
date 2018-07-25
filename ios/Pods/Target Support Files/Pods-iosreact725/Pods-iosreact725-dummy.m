2f57a67857048fd0181ff8322927d07bfed70932
bys <bys@bysdeMBP-2.guest.corp.microsoft.com>
2018-07-25 17:18:28 +0800

    upload

---
+++ ios/Pods/Target Support Files/Pods-iosreact725/Pods-iosreact725-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iosreact725 : NSObject
+@end
+@implementation PodsDummy_Pods_iosreact725
+@end
---
+++ ios/Pods/Target Support Files/Pods-iosreact725Tests/Pods-iosreact725Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iosreact725Tests : NSObject
+@end
+@implementation PodsDummy_Pods_iosreact725Tests
+@end
