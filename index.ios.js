e07cb14bca971c9185eff02a9af0bdbe15430102
bys <bys@bysdeMBP-2.guest.corp.microsoft.com>
2018-08-09 15:01:57 +0800

    updatesdk1.8.0

--- index.ios.js
+++ index.ios.js
@@ -20,7 +20,7 @@ import {
 
 Analytics.trackEvent('Video clicked', { Category: 'Music', FileName: 'favorite.avi' });
 Analytics.trackEvent('nhndhjdjdryjdrjdrjdrjdr', { Category: 'jrjdrjdjdrjdrjdr', FileName: 'jryjdrjrdjdrjdjdjdrjdjdjdyjdr.avi' });
-//Analytics.trackEvent('clicked');
+
 export default class iosreact725 extends Component {
     render() {
         return (
@@ -53,36 +53,31 @@ async function onClick() {
     const installId = await AppCenter.getInstallId();
     Alert.alert(installId);
 }
-//Analytics.trackEvent('Video clicked', { Category: 'Music', FileName: 'favorite.avi' });
-//Analytics.trackEvent('clicked');
+
 Push.setListener({
-    onPushNotificationReceived: function (pushNotification) {
-      let message = pushNotification.message;
-      let title = pushNotification.title;
-  
-      if (message === null || message === undefined) {
-        // Android messages received in the background don't include a message. On Android, that fact can be used to
-        // check if the message was received in the background or foreground. For iOS the message is always present.
-        title = "Android background";
-        message = "<empty>";
-      }
-  
-      // Custom name/value pairs set in the App Center web portal are in customProperties
-      if (pushNotification.customProperties && Object.keys(pushNotification.customProperties).length > 0) {
-        message += '\nCustom properties:\n' + JSON.stringify(pushNotification.customProperties);
-      }
-  
-      if (AppState.currentState === 'active') {
-        Alert.alert(title, message);
-      }
-      else {
-        // Sometimes the push callback is received shortly before the app is fully active in the foreground.
-        // In this case you'll want to save off the notification info and wait until the app is fully shown
-        // in the foreground before displaying any UI. You could use AppState.addEventListener to be notified
-        // when the app is fully in the foreground.
-      }
+  onPushNotificationReceived: function (pushNotification) {
+    let message = pushNotification.message;
+    let title = pushNotification.title;
+
+    if (message === null || message === undefined) {
+      // Android messages received in the background don't include a message. On Android, that fact can be used to
+      // check if the message was received in the background or foreground. For iOS the message is always present.
+      title = 'Android background';
+      message = '<empty>';
     }
-  });
+
+    // Custom name/value pairs set in the App Center web portal are in customProperties
+    if (pushNotification.customProperties && Object.keys(pushNotification.customProperties).length > 0) {
+      message += '\nCustom properties:\n' + JSON.stringify(pushNotification.customProperties);
+    }
+
+    if (AppState.currentState === 'active') {
+      Alert.alert(title, message);
+    }
+    else {
+    }
+  }
+});
   
 const styles = StyleSheet.create({
   container: {
